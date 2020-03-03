a3eb16fdc5ddff5486ff808b6b3461f55c19b757
Gozdinhos <gozdekaraozkok00@gmail.com>
2020-03-03 19:18:18 +0300

    Updates

---
+++ precache-manifest.07ca3dee0a98e5ae7b662101f12ef826.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a17a4d4c6850969546c7973fcdc5080d",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "f811ff73bfbf36985733",
+    "url": "/react-todo/static/css/main.0069d8e9.chunk.css"
+  },
+  {
+    "revision": "a8afdcbe464c57dc4f70",
+    "url": "/react-todo/static/js/2.d03c0031.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/react-todo/static/js/2.d03c0031.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f811ff73bfbf36985733",
+    "url": "/react-todo/static/js/main.35e3321d.chunk.js"
+  },
+  {
+    "revision": "ddf9db6d5a64987ad38f",
+    "url": "/react-todo/static/js/runtime-main.fe75d6e3.js"
+  }
+]);
